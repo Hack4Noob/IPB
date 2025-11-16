5a60be8bf2cec26b839e8cac976eade0eea8c5e1
Hack4Noob <134813495+Hack4Noob@users.noreply.github.com>
2025-11-16 12:53:56 +0100

    Atualizar o app

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Features />
+      {/* Outros componentes ou seções podem ser adicionados aqui */}
+    </div>
+  );
+};
+
+export default HomePage;
